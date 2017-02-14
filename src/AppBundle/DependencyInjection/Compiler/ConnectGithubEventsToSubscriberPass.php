a67eb7c6a8f02feaf7a4a13da04618e08d856827
Tomasz Wójcik <tomasz.prgtw.wojcik@gmail.com>
2017-02-14 02:25:53 +0100

    pushing github events via pusher

---
+++ src/AppBundle/DependencyInjection/Compiler/ConnectGithubEventsToSubscriberPass.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+
+class ConnectGithubEventsToSubscriberPass implements CompilerPassInterface
+{
+	/** {@inheritdoc} */
+	public function process(ContainerBuilder $container)
+	{
+		$subscribedEvents = $container->getParameter('app.github.subscribed_events');
+		$githubSubscriber = $container->findDefinition('subscriber.github');
+		foreach ($subscribedEvents as $subscribedEvent)
+		{
+			$githubSubscriber->addTag('kernel.event_listener', [
+				'event'  => "github.{$subscribedEvent}",
+				'method' => 'onGithubEvent',
+			]);
+		}
+	}
+}
---
+++ src/AppBundle/Subscriber/GithubSubscriber.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppBundle\Subscriber;
+
+use AppBundle\Event\GitHub\GitHubEventInterface;
+
+class GithubSubscriber
+{
+	/** @var \Pusher */
+	private $pusher;
+
+	public function __construct(\Pusher $pusher)
+	{
+		$this->pusher = $pusher;
+	}
+
+	public function onGithubEvent(GitHubEventInterface $event)
+	{
+		$this->pusher->trigger(['public'], $event::getEventName(), [
+			'event'   => $event::getGitHubEventType(),
+			'payload' => $event->getPayload(),
+		]);
+	}
+}
